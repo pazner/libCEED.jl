dee101e38e651f62744cf3034729167dc58b2cc6
Will Pazner <will.e.p@gmail.com>
2020-08-26 20:19:24 -0700

    Slightly modify ex2 Q-functions

--- examples/ex2-qfunction.jl
+++ examples/ex2-qfunction.jl
@@ -1,4 +1,5 @@
 using StaticArrays
+using Base: @propagate_inbounds
 
 # A structure used to pass additional data to f_build_mass
 mutable struct BuildContext
@@ -6,7 +7,7 @@ mutable struct BuildContext
     space_dim::CeedInt
 end
 
-function wdetJJinvJinvT(Q, J, w, qdata, D::CeedDim{dim}) where dim
+@propagate_inbounds function wdetJJinvJinvT(Q, J, w, qdata, D::CeedDim{dim}) where dim
     for i=1:Q
         Ji = SMatrix{dim,dim}(@view(J[i,:,:]))
         Jinv = inv(Ji)
@@ -20,14 +21,15 @@ function f_build_diff(
         ctx::BuildContext, Q,
         J::(:in, Q, ctx.dim, ctx.dim), w::(:in, Q),
         qdata::(:out, Q, ctx.dim*(ctx.dim+1)÷2))
-    @withdim (D=ctx.dim) wdetJJinvJinvT(Q, J, w, qdata, D)
+    @withdim (D=ctx.dim) @inbounds wdetJJinvJinvT(Q, J, w, qdata, D)
     CeedInt(0)
 end)
 
-function dXdxdXdxTdu(Q, ug, qdata, vg, D::CeedDim{dim}) where dim
+@propagate_inbounds function dXdxdXdxTdu(Q, ug, qdata, vg, D::CeedDim{dim}) where dim
     for i=1:Q
         dXdxdXdxT = getvoigt(@view(qdata[i,:]), D)
-        vg[i,:] .= dXdxdXdxT*@view(ug[i,:])
+        ugi = SVector{dim}(@view(ug[i,:]))
+        vg[i,:] .= dXdxdXdxT*ugi
     end
 end
 
@@ -37,6 +39,6 @@ function f_apply_diff(
         ctx::BuildContext, Q,
         ug::(:in, Q, ctx.dim), qdata::(:in, Q, ctx.dim*(ctx.dim+1)÷2),
         vg::(:out, Q, ctx.dim))
-    @withdim (D=ctx.dim) dXdxdXdxTdu(Q, ug, qdata, vg, D)
+    @withdim (D=ctx.dim) @inbounds dXdxdXdxTdu(Q, ug, qdata, vg, D)
     CeedInt(0)
 end)
