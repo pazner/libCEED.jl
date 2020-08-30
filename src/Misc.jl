5adb7f2852f3a999b3f82f9dc113d7b89ac275c7
Will Pazner <will.e.p@gmail.com>
2020-08-30 13:20:16 -0700

    Make `det` inline

--- src/Misc.jl
+++ src/Misc.jl
@@ -3,11 +3,11 @@ import LinearAlgebra: det
 struct CeedDim{dim} end
 CeedDim(dim) = CeedDim{Int(dim)}()
 
-det(J, ::CeedDim{1}) = @inbounds J[1]
+@inline det(J, ::CeedDim{1}) = @inbounds J[1]
 
-det(J, ::CeedDim{2}) = @inbounds J[1]*J[4] - J[3]*J[2]
+@inline det(J, ::CeedDim{2}) = @inbounds J[1]*J[4] - J[3]*J[2]
 
-det(J, ::CeedDim{3}) = @inbounds (J[1]*(J[5]*J[9] - J[6]*J[8]) -
+@inline det(J, ::CeedDim{3}) = @inbounds (J[1]*(J[5]*J[9] - J[6]*J[8]) -
     J[2]*(J[4]*J[9] - J[6]*J[7]) +
     J[3]*(J[4]*J[8] - J[5]*J[7]))
 
